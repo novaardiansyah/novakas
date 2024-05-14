06a0f502ec4b1b553651c600c992a76a70283269
novaardiansyah <novaardiansyah78@gmail.com>
2024-05-15 00:48:27 +0700

    Financial history form improvement:
    * Current finances are linked to the cash account, each amount will be recorded in the respective cash account.

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -6,12 +6,18 @@ use App\Filament\Resources\PaymentResource\Pages;
 use App\Models\Payment;
 use App\Models\PaymentAccount;
 use Carbon\Carbon;
+use Filament\Actions\EditAction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Log\Logger;
+
+use function Filament\Support\format_number;
 
 class PaymentResource extends Resource
 {
@@ -38,41 +44,60 @@ class PaymentResource extends Resource
             ->description('Detail pencatatan saldo keuangan.')
             ->columns(2)
             ->schema([
-              Forms\Components\TextInput::make('name')
-                ->required(),
               Forms\Components\TextInput::make('amount')
+                ->label('Nominal')
                 ->required()
-                ->numeric(),
+                ->numeric()
+                ->step(1000),
               Forms\Components\DatePicker::make('date')
+                ->label('Tanggal')
                 ->required()
                 ->default(Carbon::now())
                 ->native(false),
+              Forms\Components\Textarea::make('name')
+                ->label('Catatan')
+                ->nullable()
+                ->columnSpanFull()
+                ->rows(3),
             ]),
         ])
         ->columnSpan(2),
 
         Forms\Components\Group::make([
+          Forms\Components\Section::make('Jenis Transaksi')
+            ->collapsible()
+            ->schema([
+              Forms\Components\Select::make('type_id')
+                ->label('Tipe Transaksi')
+                ->relationship('payment_type', titleAttribute: 'name')
+                ->required()
+                ->default(2)
+                ->disabledOn('edit')
+                ->native(false)
+            ]),
+
           Forms\Components\Section::make('Akun Kas')
-          ->description('Pilih akun kas yang akan digunakan.')
-          ->columns(1)
-          ->schema([
-            Forms\Components\Select::make('payment_account.name')
-              ->label('Akun Kas')
-              ->relationship('payment_account', titleAttribute: 'name')
-              ->searchable()
-              ->preload()
-              ->live()
-              ->required()
-              ->afterStateUpdated(function(Forms\Set $set, string $state, string $operation) {
-                $payment_account = PaymentAccount::find($state);
-                if ($operation === 'create') {
-                  $set('payment_account.deposit', 'Rp ' . number_format($payment_account->deposit, 2, ',', '.'));
-                }
-              }),
-            Forms\Components\TextInput::make('payment_account.deposit')
-              ->disabled()
-              ->default(0),
-          ]),
+            ->collapsible()
+            ->schema([
+              Forms\Components\Select::make('payment_account_id')
+                ->label('Akun Kas')
+                ->relationship('payment_account', titleAttribute: 'name')
+                ->searchable()
+                ->preload()
+                ->live()
+                ->required()
+                ->disabledOn('edit')
+                ->afterStateUpdated(function(Forms\Set $set, string $state, string $operation) {
+                  $payment_account = PaymentAccount::find($state);
+                  if ($operation === 'create') {
+                    $set('payment_account_deposit', 'Rp ' . number_format($payment_account->deposit, 0, ',', '.'));
+                  }
+                }),
+              Forms\Components\TextInput::make('payment_account_deposit')
+                ->label('Saldo')
+                ->disabled()
+                ->default(0),
+            ]),
         ])
         ->columnSpan(1)
       ]);
@@ -88,7 +113,37 @@ class PaymentResource extends Resource
         //
       ])
       ->actions([
-        Tables\Actions\EditAction::make(),
+        Tables\Actions\EditAction::make()
+          ->mutateRecordDataUsing(function (array $data): array {
+            $payment_account = PaymentAccount::find($data['payment_account_id']);
+            $data['payment_account_deposit'] = 'Rp. ' . number_format($payment_account->deposit, 0, ',', '.');
+            return $data;
+          })
+          ->before(function (Model $record, array $data, $action): array {
+            $amount = $data['amount'] ?? 0;
+
+            $payment_account = PaymentAccount::find($record->payment_account_id);
+
+            if ($record->type_id == 2) {
+              $newDeposit = $payment_account->deposit + $record->amount;
+
+              if ($newDeposit < $amount) {
+                Notification::make()
+                  ->danger()
+                  ->title('Proses gagal!')
+                  ->body('Saldo akun kas tidak mencukupi.')
+                  ->send();
+                $action->halt();
+              }
+
+              $payment_account->deposit = $newDeposit - $amount;
+            } else {
+              $payment_account->deposit = $payment_account->deposit - $record->amount + $amount;
+            }
+
+            $payment_account->save();
+            return $data;
+          }),
       ])
       ->bulkActions([
         Tables\Actions\BulkActionGroup::make([
@@ -107,9 +162,9 @@ class PaymentResource extends Resource
   public static function getPages(): array
   {
     return [
-      'index' => Pages\ListPayments::route('/'),
+      'index'  => Pages\ListPayments::route('/'),
       'create' => Pages\CreatePayment::route('/create'),
-      'edit' => Pages\EditPayment::route('/{record}/edit'),
+      // 'edit' => Pages\EditPayment::route('/{record}/edit'),
     ];
   }
 }
